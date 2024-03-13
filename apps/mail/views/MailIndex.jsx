6bbf3918ed84b3b9b059820a4cb2c6b17fdce20c
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-03-13 12:20:39 +0200

    Move storageService to UtilService

--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,6 +1,18 @@
+const { useState, useEffect } = React
+
+import { emailService } from '../services/mail.service.js'
+
 import { MailList } from '../cmps/MailList.jsx'
 
 export function MailIndex() {
+  useEffect(() => {
+    loadMails()
+  }, [])
+
+  function loadMails() {
+    emailService.query().then(console.log).catch()
+  }
+
   return (
     <section className="mail-index">
       <MailList />
